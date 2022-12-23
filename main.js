cd8cfddd02e58756c401585c159474418f1cbba9
lowc0ff <lowc0ff@protonmail.com>
2022-12-23 11:07:51 -0300

    day3

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-const somPom = () => {
-    document.querySelector('#som_tecla_pom').play();
+const tocaSom = (idElementAudio) => {
+    document.querySelector(idElementAudio).play();
 }
 
-const ListaDeTeclas = document.querySelectorAll('.tecla');
-const ListaDeSom = document.querySelectorAll('.som');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let index = 0
-while(index < ListaDeTeclas.lenght) {
-    ListaDeTeclas[index].onclick = ListaDeSom[index]
-    index++
+let index = 0;
+while(index < listaDeTeclas.length) {
+
+    const teclas = listaDeTeclas[index];
+
+    const instrumento = teclas.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    teclas.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    teclas.onkeydown = () => {
+        teclas.classList.add('ativa')
+    }
+    teclas.onkeyup = () => {
+        teclas.classList.remove('ativa')
+    }
+
+    index = index + 1;
 }
